c0daaa80c2313e04ae94d77d48859bd193131ea0
peaceiris <su.peaceiris@gmail.com>
2017-08-23 02:54:14 +0900

    Add: map3.js

---
+++ js/map3.js
@@ -0,0 +1,32 @@
+var window_obj = null;
+
+function gotoMap() {
+     window_obj = window.open('map3.html', '_blank', 'width=400, height=400, menubar=no, toolbar=no, scrollbars=yes');
+
+}
+
+function closeMap() {
+    if(window.opener){
+        // ウィンドウを閉じる
+//        window.close();
+            window.open('after_map3.html', '_self', 'width=400, height=400, menubar=no, toolbar=no, scrollbars=yes');
+    }
+}
+
+// ------------------------------------------------------------
+// 一定の時間隔で実行する
+// ------------------------------------------------------------
+// var interval = setInterval(function () {
+//     if (window_obj) {
+//         // ウィンドウが閉じられたか調べる
+//         var closed = window_obj.closed;
+
+//         // 出力テスト
+//         if (closed) {
+//             console.log("Window was closed." + closed);
+//             clearInterval( interval );
+//             window.open('html/after_map1.html', '_self', 'width=400, height=400, menubar=no, toolbar=no, scrollbars=yes');
+//         }
+
+//     }
+// },1000);
