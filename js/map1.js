219fa3e0c62cb117969e5baf6ca55ec7ecd57641
peaceiris <su.peaceiris@gmail.com>
2017-08-23 01:47:38 +0900

    Modify: move to after_map1.html from map1.html

--- js/map1.js
+++ js/map1.js
@@ -8,24 +8,25 @@ function gotoMap1() {
 function closeMap1() {
     if(window.opener){
         // ウィンドウを閉じる
-        window.close();
+//        window.close();
+            window.open('after_map1.html', '_self', 'width=400, height=400, menubar=no, toolbar=no, scrollbars=yes');
     }
 }
 
 // ------------------------------------------------------------
 // 一定の時間隔で実行する
 // ------------------------------------------------------------
-var interval = setInterval(function () {
-    if (window_obj) {
-        // ウィンドウが閉じられたか調べる
-        var closed = window_obj.closed;
+// var interval = setInterval(function () {
+//     if (window_obj) {
+//         // ウィンドウが閉じられたか調べる
+//         var closed = window_obj.closed;
 
-        // 出力テスト
-        if (closed) {
-            console.log("Window was closed." + closed);
-            clearInterval( interval );
-            window.open('html/after_map1.html', '_self', 'width=400, height=400, menubar=no, toolbar=no, scrollbars=yes');
-        }
+//         // 出力テスト
+//         if (closed) {
+//             console.log("Window was closed." + closed);
+//             clearInterval( interval );
+//             window.open('html/after_map1.html', '_self', 'width=400, height=400, menubar=no, toolbar=no, scrollbars=yes');
+//         }
 
-    }
-},1000);
+//     }
+// },1000);
